658b63643b340b22e71349f2855a776f0a1eebbb
CharlesPBiggar <charles.p.biggar@gmail.com>
2020-03-17 18:54:35 -0400

    added Bcrypt for hashing user passwords

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,6 +3,7 @@
 # Dependencies
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -11,5 +12,8 @@ app.config['SECRET_KEY'] = 'e94f492957e9be802f10ca325ee12cb3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+
+# used for protecting user passwords
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
