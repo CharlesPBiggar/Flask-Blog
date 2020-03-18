d608ffd61b4ac653b8ba97a35b15ee7a75f603d9
CharlesPBiggar <charles.p.biggar@gmail.com>
2020-03-18 17:35:16 -0400

    added registration validation system to check db to ensure uniqueness of usernames and emails

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -2,7 +2,8 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
@@ -15,6 +16,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                             validators=[DataRequired(), Length(min=6, max=30), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose another username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is already in use. Please choose another email or try signing in.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', 
