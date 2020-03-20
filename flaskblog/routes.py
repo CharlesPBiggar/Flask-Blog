3057e726e311d4846c219c836b878a1763b67a4d
CharlesPBiggar <charles.p.biggar@gmail.com>
2020-03-20 18:04:12 -0400

    added ,first() to handle BaseQuery error i was getting on loging in

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -52,7 +52,7 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data)
+        user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
