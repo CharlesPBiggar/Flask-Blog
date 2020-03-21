5898576cd83deeacad27dd99be36f158780bcb56
CharlesPBiggar <charles.p.biggar@gmail.com>
2020-03-21 16:56:42 -0400

    added new form to update user account from the account page

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,8 @@
 # Forms Page
 
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp, ValidationError
 from flaskblog.models import User
@@ -27,6 +29,7 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('That email is already in use. Please choose another email or try signing in.')
 
+
 class LoginForm(FlaskForm):
     email = StringField('Email', 
                         validators=[DataRequired(), Email()])
@@ -34,3 +37,25 @@ class LoginForm(FlaskForm):
                             validators=[DataRequired(), Length(min=6, max=30)])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class UpdateAccoutForm(FlaskForm):
+    username = StringField('Username', 
+                            validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', 
+                            validators=[DataRequired(), Regexp(regex=r'^[A-Za-z0-9\-\.]+@[A-Za-z\-]+.[a-z]{2,3}$', message='Not a valid Email.')])
+                                                                        # Created Regular Expression to catch entrys that are not emails
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+ 
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose another username.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is already in use. Please choose another email or try signing in.')
